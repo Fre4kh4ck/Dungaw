7c828b95309883b1535ccd992d539bb34b7ead18
Chad Fernandez <developer@chadfernandez.me>
2025-11-19 16:52:27 +0800

    add initial database schema and docker-compose configuration

--- api/db.js
+++ api/db.js
@@ -4,9 +4,11 @@ const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
 });
 
 module.exports = Object.freeze({ pool: pool });
